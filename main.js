f8a5fc16765bcf7e658e120627fa9964c2195d78
Vert <conflict@email.com>
2023-02-22 20:31:17 -0500

    sleep to bypass chrome dl limit;

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-//Sets up download link
 const dllink = window.location.href;
 var splitlink = dllink.split('/');
 const daymonth = splitlink[6];
 const year = splitlink[5];
-
+ 
 //Searches pdf for keyword 
 const input = document.querySelector('.flowpaper_txtSearch'); // Finds searchbar
 const search = document.querySelector('.flowpaper_bttnFind'); // Finds Search button1
-input.value = "CI$"; // Inputs value into search bar
+input.value = "Employment Vacancies"; // Inputs value into search bar
 search.click(); 
+ 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
  
 const res = []; const contentChildren = document.querySelector(".flowpaper_searchabstracts_content").children;
 const childrenArray = Array.from(contentChildren); 
@@ -16,10 +19,29 @@ const childrenArray = Array.from(contentChildren);
 childrenArray.forEach((element) => { 
   const bTag = element.querySelector("b"); 
   if(bTag){ const bText = bTag.innerText; 
-           const pages = bText.substr(2);
+           const pages = bText.substr(3);
            res.push(pages); 
           }
 }); 
+ 
+await sleep(1000)
+input.value = "CI$"; // Inputs value into search bar
+search.click(); 
+ 
+const contentChildren2 = document.querySelector(".flowpaper_searchabstracts_content").children;
+const childrenArray2 = Array.from(contentChildren2); 
+childrenArray2.forEach((element) => { 
+const bTag = element.querySelector("b"); 
+  if(bTag){ 
+	const bText = bTag.innerText; 
+          const pages = bText.substr(3);
+	if (pages > res[0]){
+           res.push(pages); 
+	}
+          }
+}); 
+ 
+ 
 const unique = [...new Set(res)]; //Filters duplicate values
 const min = Math.min(...unique);
 const max = Math.max(...unique);
@@ -30,9 +52,9 @@ z++;
 }
  
 function download(){
-    let url = 'https://www.caymancompass.com/updates/eversion/'+ year + '/' + daymonth + '/docs/'+ year + daymonth + '_' + unique[i] + '.pdf';
+    let url = 'https://www.cc.com/updates/eversion/'+ year + '/' + daymonth + '/docs/'+ year + daymonth + '_' + unique[i] + '.pdf';
     const link = document.createElement("a");
-    link.download = "Classified_" + unique[i] + ".pdf";
+    link.download = "Classified" + year + daymonth + "_" + unique[i] + ".pdf";
     link.href = url;
     document.body.appendChild(link);
     link.click();
@@ -43,4 +65,5 @@ var i = 0;
  while (i < unique.length) {
 	download();
 	i++;
+	await sleep(800);
 }
